73d84b98f01ce614ddfaa2e2be9eaf1d0ff0e6a4
vb <verabertalan@gmail.com>
2021-02-03 16:35:28 +0100

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
 
 
 class App extends Component{
   render() {
     return (
       <div className="App">
-        <h1>hell mami</h1>
+        <div className="Container">
+          <Navbar />
+          <h1>hell mami</h1>
+        </div>
       </div>
     );
   }
