d76ff70bf7d0d9062e78a0e9582e92ede877bd86
vb <verabertalan@gmail.com>
2021-02-03 16:51:46 +0100

    add PokemonList component to its route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from './components/layout/Navbar';
+import PokemonList from './components/PokemonList'
 
 
 class App extends Component{
   render() {
     return (
-      <div className="App">
-        <h1>hell mami</h1>
-      </div>
+      <Router>
+        <div className="App">	
+            <Navbar />
+            <Route exact path="/" render={(props) => (
+              <React.Fragment>
+                <p>main page</p>
+              </React.Fragment>
+            )} />
+            <Route path="/pokemons" component={PokemonList} />
+            <Route path="/types"  />
+        </div>
+      </Router>
     );
   }
 }
