c44a0bf2597a456f8612a8a21f40432c66cd28f5
vb <verabertalan@gmail.com>
2021-02-04 16:39:01 +0100

    getting fetchedData in PokemonListItem with useHttp

--- src/components/PokemonListItem.js
+++ src/components/PokemonListItem.js
@@ -1,29 +1,33 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { useHttp } from "../hooks/Http";
 import { Link } from "react-router-dom";
-import axios from "axios";
 
 const PokemonListItem = (props) => {
-  const [image, setImage] = useState("");
-  const [id, setId] = useState(0);
+  const [isLoading, fetchedData] = useHttp(props.pokemon.url, []);
 
-  useEffect(() => {
-    axios
-      .get(props.pokemon.url)
-      .then(
-        (response) =>
-          setImage(response.data.sprites.other.dream_world.front_default) +
-          setId(response.data.id)
-      );
-  }, [props.pokemon.url]);
+  const pokemon = fetchedData
+    ? {
+        image: fetchedData.data.sprites.other.dream_world.front_default,
+        id: fetchedData.data.id,
+      }
+    : [];
 
-  return (
-    <li>
-      <Link to={`pokemon/${id}`}>
-        <img src={image} alt="poke" />
-        <p>{props.pokemon.name}</p>
-      </Link>
-    </li>
-  );
+  if (isLoading && fetchedData) {
+    return (
+      <li>
+        <Link to={`pokemon/${pokemon.id}`}>
+          <img src={pokemon.image} alt="poke" />
+          <p>{props.pokemon.name}</p>
+        </Link>
+      </li>
+    );
+  } else {
+    return (
+      <div>
+        <p>Getting pokemon..</p>
+      </div>
+    );
+  }
 };
 
 export default PokemonListItem;
